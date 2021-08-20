9dfd42a6f461f27a1014ec6421fa596989d9ad1b
Volodia <volodymyr.m@eivolo.com>
2021-08-20 11:43:48 +0300

    added AdminLayout

---
+++ pages/admin/index.tsx
@@ -0,0 +1,12 @@
+import { withAdminLayout } from '../../layouts/admin/AdminLayout';
+
+function Dashboard(): JSX.Element {
+	return (
+		<>
+			<h1>Dashboard</h1>
+
+		</>
+	);
+}
+
+export default withAdminLayout(Dashboard);
---
+++ pages/admin/users.tsx
@@ -0,0 +1,12 @@
+import { withAdminLayout } from '../../layouts/admin/AdminLayout';
+
+function Users(): JSX.Element {
+	return (
+		<>
+			<h1>Users page</h1>
+
+		</>
+	);
+}
+
+export default withAdminLayout(Users);
