33895984112b1c2a64995d63b07c8ca6d8a89a18
Volodia <volodymyr.m@eivolo.com>
2021-08-30 12:16:18 +0300

    implemented field sorting for table (admin/messages)

--- components/MessageList/message-list.props.ts
+++ components/MessageList/message-list.props.ts
@@ -1,6 +1,7 @@
 import { DetailedHTMLProps, HTMLAttributes } from "react";
-import IMessage from '../../interfaces/message.interface';
+import { IMessage } from '../../interfaces/message.interface';
 
 export interface MessageListProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
 	messages: IMessage[];
+	sortByName: () => void;
 }
